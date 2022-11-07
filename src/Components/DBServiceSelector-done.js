6fb87c757e9a0f782db443a29400ccd58452c89e
Brian Staman Ogilvie <brianogilvie@fb.com>
2022-11-07 09:47:54 -0500

    service selector test

---
+++ src/Components/DBServiceSelector-done.js
@@ -0,0 +1,19 @@
+import React, { useCallback } from "react";
+
+export default function DBServiceSelector({services, serviceID, setServiceID}) {
+
+  const handleChange = useCallback(({target}) => {
+    setServiceID(parseInt(target.value, 10));
+  }, [setServiceID]);
+
+  return (
+    <label htmlFor="services-selector">
+      Database Service
+      <select id="services-selector" value={serviceID} onChange={handleChange}>
+        {services.map(({service_id, service_name}) => (
+          <option key={service_id} value={service_id}>{service_name}</option>
+        ))}
+      </select>
+    </label>
+  )
+}
--- src/Components/__tests__/DBServiceSelector.test.js
+++ src/Components/__tests__/DBServiceSelector.test.js
@@ -1,41 +1,52 @@
-import DBServiceSelector from '../DBServiceSelector';
-import { act, cleanup, render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import DBServiceSelector from '../DBServiceSelector-done';
 
-const mockGetDatabaseServices = jest.fn();
-
-jest.mock('../../Services/API', () => ({
-  getDatabaseServices: () => mockGetDatabaseServices,
-}));
+import * as React from 'react';
+import {cleanup, fireEvent, render, screen} from '@testing-library/react';
 
 const SERVICES = [
   {
-    id: 1,
+    service_id: 1,
+    service_name: 'SQL Server',
+  },
+  {
+    service_id: 2,
     service_name: 'MySQL',
-  },
-  {
-    id: 2,
-    service_name: 'MongoDB',
-  },
-  {
-    id: 3,
-    service_name: 'Cassandra',
   },
 ];
 
 describe('DBServiceSelector', () => {
+  const mockSetServiceID = jest.fn();
 
-  beforeEach(jest.clearAllMocks);
+  function TestRenderer() {
+    return (
+      <DBServiceSelector
+        services={SERVICES}
+        serviceID={0}
+        setServiceID={mockSetServiceID}
+      />
+    );
+  }
 
+  beforeEach(() => {
+    mockSetServiceID.mockClear();
+  });
+  
   afterEach(cleanup);
   
-  test('renders loading indicator', () => {
-    render(<DBServiceSelector />);
-    expect(screen.queryByText('Loading')).toBeVisible();
+  test('should render list of services', () => {
+    render(<TestRenderer />);
+    expect(screen.getByLabelText('Database Service')).toBeVisible();
+    SERVICES.forEach(({service_name}) => {
+      expect(screen.getByText(service_name)).toBeInTheDocument();
+    });
   });
 
-  test('calls the API on render', async () => {
-    mockGetDatabaseServices.mockResolvedValueOnce(SERVICES);
-    render(<DBServiceSelector />);
-    expect(mockGetDatabaseServices).toHaveBeenCalled();
+  test('should update selected value when user clicks an option', () => {
+    render(<TestRenderer />);
+    const selector = screen.getByLabelText('Database Service');
+    SERVICES.forEach(({service_id}) => {
+      fireEvent.change(selector, {target: {value: service_id}});
+      expect(mockSetServiceID).toHaveBeenCalledWith(service_id);
+    });
   });
-})
+});
